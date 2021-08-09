ff92f1bc7ae2b0de71fea6847884d710d6f11419
SEB <se13codeur@gmail.com>
2021-08-09 14:18:17 +0200

    modified config + added models

--- config/db.js
+++ config/db.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(
-    'mongodb+srv://SEB:SE13codeur@cluster0.5hpct.mongodb.net/SocialNetwork',
+    'mongodb+srv://' + process.env.DB_USER_PASS + '@cluster0.5hpct.mongodb.net/SocialNetwork',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        }
+    )
     
-)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("Failed to connect to MongoDB", err))
