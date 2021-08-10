fc7c82bf0017ca621cd90bc5e1590d62a50a3c6d
SEB <se13codeur@gmail.com>
2021-08-10 10:11:53 +0200

    added uploads in client/public

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,8 +1,8 @@
-const UserModel = require("../models/user.model");
-const fs = require("fs");
-const { promisify } = require("util");
-const pipeline = promisify(require("stream").pipeline);
-const { uploadErrors } = require("../utils/errors.utils");
+const UserModel = require("../models/user.model")
+const fs = require("fs")
+const { promisify } = require("util")
+const pipeline = promisify(require("stream").pipeline)
+const { uploadErrors } = require("../utils/errors.utils")
 
 module.exports.uploadProfil = async (req, res) => {
   try {
@@ -11,21 +11,21 @@ module.exports.uploadProfil = async (req, res) => {
       req.file.detectedMimeType != "image/png" &&
       req.file.detectedMimeType != "image/jpeg"
     )
-      throw Error("invalid file");
+      throw Error("invalid file")
 
-    if (req.file.size > 500000) throw Error("max size");
+    if (req.file.size > 500000) throw Error("max size")
   } catch (err) {
-    const errors = uploadErrors(err);
-    return res.status(201).json({ errors });
+    const errors = uploadErrors(err)
+    return res.status(201).json({ errors })
   }
-  const fileName = req.body.name + ".jpg";
+  const fileName = req.body.name + ".jpg"
 
   await pipeline(
     req.file.stream,
     fs.createWriteStream(
       `${__dirname}/../client/public/uploads/profil/${fileName}`
     )
-  );
+  )
 
   try {
     await UserModel.findByIdAndUpdate(
@@ -33,11 +33,11 @@ module.exports.uploadProfil = async (req, res) => {
       { $set : {picture: "./uploads/profil/" + fileName}},
       { new: true, upsert: true, setDefaultsOnInsert: true},
       (err, docs) => {
-        if (!err) return res.send(docs);
-        else return res.status(500).send({ message: err });
+        if (!err) return res.send(docs)
+        else return res.status(500).send({ message: err })
       }
-    );
+    )
   } catch (err) {
-    return res.status(500).send({ message: err });
+    return res.status(500).send({ message: err })
   }
-};
+}
--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -1,19 +1,19 @@
 module.exports.signUpErrors = (err) => {
-    let errors = { pseudo: "", email: "", password: "" }
+    let errors = { username: "", email: "", password: "" }
   
-    if (err.message.includes("pseudo"))
-      errors.pseudo = "Pseudo incorrect ou déjà pris"
+    if (err.message.includes("username"))
+      errors.username = "Incorrect username or already token"
   
-    if (err.message.includes("email")) errors.email = "Email incorrect"
+    if (err.message.includes("email")) errors.email = "Incorrect email"
   
     if (err.message.includes("password"))
-      errors.password = "Le mot de passe doit faire 6 caractères minimum"
+      errors.password = "Password must be at least 6 characters long"
   
-    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("pseudo"))
-      errors.pseudo = "Ce pseudo est déjà pris"
+    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("username"))
+      errors.username = "Username already token"
   
     if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("email"))
-      errors.email = "Cet email est déjà enregistré"
+      errors.email = "This email is already registered "
   
     return errors
   }
@@ -22,10 +22,10 @@ module.exports.signUpErrors = (err) => {
     let errors = { email: '', password: ''}
   
     if (err.message.includes("email")) 
-      errors.email = "Email inconnu"
+      errors.email = "Email unknown"
     
     if (err.message.includes('password'))
-      errors.password = "Le mot de passe ne correspond pas"
+      errors.password = "Password does not match"
   
     return errors
   }
@@ -34,10 +34,10 @@ module.exports.signUpErrors = (err) => {
     let errors = { format: '', maxSize: ""}
   
     if (err.message.includes('invalid file'))
-      errors.format = "Format incompatabile"
+      errors.format = "Inconsistent format"
   
     if (err.message.includes('max size'))
-      errors.maxSize = "Le fichier dépasse 500ko"
+      errors.maxSize = "File exceeds 500ko"
   
     return errors
   }
